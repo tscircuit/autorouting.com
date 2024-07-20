ce58f0c488e1f599ab49f4cc0088c969b78bbc8c
seveibar <seveibar@gmail.com>
2024-07-20 11:42:37 -0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import ardot from "/images/ardot.png"
+import Image from "next/image.js"
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>autorouting</h1>
+      <Image alt="autorouting text logo" src={ardot} />
+      <p>
+        Autorouting is the process of drawing wires on a printed circuit board
+        in order to connect all the components (chips) of a circuit.
+      </p>
+      <p>
+        We're releasing <a href="https://github.com/tscircuit/autorouting-datasets">open datasets and benchmarks</a> for autorouting. You should <a href="https://blog.autorouting.com">subscribe on Substack</a> to hear about big releases (expect something ~once a month)
+      </p>
     </main>
   );
 }
