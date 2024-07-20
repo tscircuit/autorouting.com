b95ba154f87dcf7de94a359307fe8d4c5733075c
seveibar <seveibar@gmail.com>
2024-07-20 12:05:44 -0700

    Replaced the ardot.png image with ardot.svg in the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import ardot from "/images/ardot.png"
+import ardot from "/images/ardot.svg"
 import Image from "next/image.js"
 
 export default function Home() {
   return (
     <main>
+      <div className="container mx-auto">
       <h1>autorouting</h1>
       <Image alt="autorouting text logo" src={ardot} />
       <p>
@@ -13,6 +14,7 @@ export default function Home() {
       <p>
         We're releasing <a href="https://github.com/tscircuit/autorouting-datasets">open datasets and benchmarks</a> for autorouting. You should <a href="https://blog.autorouting.com">subscribe on Substack</a> to hear about big releases (expect something ~once a month)
       </p>
+      </div>
     </main>
   );
 }
