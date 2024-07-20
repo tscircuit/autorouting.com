e76a643672dbb3eb6352aa435a92cd74cde6834c
seveibar <seveibar@gmail.com>
2024-07-20 12:06:43 -0700

    fix link to dataset

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           in order to connect all the components (chips) of a circuit.
         </p>
         <p className="text-lg mb-4 text-gray-700">
-          We're releasing <a href="https://github.com/tscircuit/autorouting-datasets" className="text-blue-600 hover:underline">open datasets and benchmarks</a> for autorouting. You should <a href="https://blog.autorouting.com" className="text-blue-600 hover:underline">subscribe on Substack</a> to hear about big releases (expect something ~once a month)
+          We're releasing <a href="https://github.com/tscircuit/autorouting-dataset" className="text-blue-600 hover:underline">open datasets and benchmarks</a> for autorouting. You should <a href="https://blog.autorouting.com" className="text-blue-600 hover:underline">subscribe on Substack</a> to hear about big releases (expect something ~once a month)
         </p>
       </div>
     </main>
