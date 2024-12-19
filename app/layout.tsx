8aaa722535673042ed45d9eae3eeaddfb16327fc
seveibar <seveibar@gmail.com>
2024-12-19 12:10:13 -0800

    init shadcn (fully)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "@/styles/globals.css"
 import { Header } from "@/components/header"
 
 const inter = Inter({ subsets: ["latin"] })
