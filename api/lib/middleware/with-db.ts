0c5ed0a7113f4012d3ba460ed1272bfb550e9f0a
seveibar <seveibar@gmail.com>
2024-12-19 16:34:22 -0800

    init endpoints

--- api/lib/middleware/with-db.ts
+++ api/lib/middleware/with-db.ts
@@ -1,5 +1,15 @@
+import type { DbClient } from "@/api/lib/db/db-client"
+import { createDatabase } from "@/api/lib/db/db-client"
 import type { Middleware } from "winterspec"
 
-export const withDb: Middleware<{}, {}> = async (req, ctx, next) => {
+export const withDb: Middleware<
+  {},
+  {
+    db: DbClient
+  }
+> = async (req, ctx, next) => {
+  if (!ctx.db) {
+    ctx.db = createDatabase()
+  }
   return next(req, ctx)
 }
