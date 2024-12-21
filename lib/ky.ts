e5dab471a6341cca553c87b140e4355ae3d2eca1
seveibar <seveibar@gmail.com>
2024-12-21 10:31:47 -0800

    update api base url on prod

--- lib/ky.ts
+++ lib/ky.ts
@@ -1,6 +1,9 @@
 import defaultKy from "ky"
 
-export const getBaseApiUrl = () => "http://localhost:3091"
+export const getBaseApiUrl = () =>
+  process.env.VERCEL
+    ? "https://datasets-api.autorouting.com"
+    : "http://localhost:3091"
 
 export const ky = defaultKy.extend({
   prefixUrl: getBaseApiUrl(),
